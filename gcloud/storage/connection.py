fc4f0a90c60d1e55d89d37a1b8c2650fb93bea8f
JJ Geewax <jj@geewax.org>
2014-02-02 00:15:31 -0500

    Lots of updates to connection.Connection.

--- gcloud/storage/connection.py
+++ gcloud/storage/connection.py
@@ -1,21 +1,24 @@
 import httplib2
 import json
+import urllib
 
 
 from gcloud import connection
 from gcloud.storage import exceptions
 from gcloud.storage.bucket import Bucket
+from gcloud.storage.iterator import BucketIterator
 
 
-# TODO: Document this class.
 class Connection(connection.Connection):
   """A connection to Google Cloud Storage via the JSON REST API.
 
   This class should understand only the basic types (and protobufs)
   in method arguments, however should be capable of returning advanced types.
 
-  :type credentials: :class:`gcloud.datastore.credentials.Credentials`
-  :param credentials: The OAuth2 Credentials to use for this connection.
+  See :class:`gcloud.connection.Connection` for a full list of parameters.
+
+  :type project_name: string
+  :param project_name: The project name to connect to.
   """
 
   API_VERSION = 'v1beta2'
@@ -29,9 +32,16 @@ class Connection(connection.Connection):
 
     self.project_name = project_name
 
-  # TODO: Fix these arguments. There is cruft here.
-  def make_request(self, method, path, bucket=None, key=None,
-                   data=None, api_base_url=None, api_version=None):
+  def __iter__(self):
+    return iter(BucketIterator(connection=self))
+
+  def __contains__(self, bucket_name):
+    return self.lookup(bucket_name) is not None
+
+  def make_request(self, method, path=None, url=None, query_params=None,
+                   data=None, content_type='text/plain',
+                   api_base_url=None, api_version=None,
+                   expect_json=False):
     """Make a request over the Http transport to the Cloud Datastore API.
 
     :type method: string
@@ -41,7 +51,7 @@ class Connection(connection.Connection):
     """
 
     headers = {
-        'Accept-Encoding': 'gzip',
+        'accept-encoding': 'gzip',
         }
 
     url = self.API_URL_TEMPLATE.format(
@@ -49,42 +59,49 @@ class Connection(connection.Connection):
         api_version=(api_version or self.API_VERSION),
         path=path)
 
-    # TODO: Use urlparse for this.
-    url += '?project=' + self.project_name
+    query_params = query_params or {}
+    query_params.update({'project': self.project_name})
+    url += '?' + urllib.urlencode(query_params)
 
     if data:
-      data = json.dumps(data)
-      headers.update({
-        'Content-Type': 'application/json',
-        'Content-Length': str(len(data)),
-        })
+      # Making the executive decision that any dictionary
+      # data will be JSON.
+      if isinstance(data, dict):
+        data = json.dumps(data)
+        content_type = 'application/json'
+
+      headers['content-length'] = str(len(str(data)))
+      if content_type:
+        headers['content-type'] = content_type
 
     response_headers, content = self.http.request(
         uri=url, method=method, headers=headers, body=data)
 
-    # TODO: Better error handling.
+    # TODO: Add better error handling.
     status = response_headers['status']
     if status == '404':
       raise exceptions.NotFoundError(response_headers, content)
     elif not status.startswith('2'):
-      raise exceptions.Error(response_headers, content)
+      raise exceptions.ConnectionError(response_headers, content)
 
-    return json.loads(content)
+    if not content:
+      return
 
-  def __iter__(self):
-    for bucket in self.get_all_buckets():
-      yield bucket
-
-  def __contains__(self, bucket_name):
-    return self.lookup(bucket_name) is not None
+    if expect_json:
+      # TODO: Better checking on this header for JSON.
+      content_type = response_headers.get('content-type', '')
+      if not content_type.startswith('application/json'):
+        raise TypeError('Expected JSON, got %s' % content_type)
+      return json.loads(content)
+    else:
+      return content
 
   def get_all_buckets(self, *args, **kwargs):
-    response = self.make_request(method='GET', path='/b')
-    return [Bucket.from_dict(b, connection=self) for b in response['items']]
+    return list(self)
 
   def get_bucket(self, bucket_name, *args, **kwargs):
     # TODO: URL-encode the bucket name to be safe?
-    response = self.make_request(method='GET', path='/b/%s' % bucket_name)
+    response = self.make_request(method='GET', path=self.path, expect_json=True)
     return Bucket.from_dict(response, connection=self)
 
   def lookup(self, bucket_name):
@@ -93,10 +110,30 @@ class Connection(connection.Connection):
     except exceptions.NotFoundError:
       return None
 
-  def create_bucket(self, bucket_name, *args, **kwargs):
-    response = self.make_request(method='POST', path='/b', data={'name': bucket_name})
+  def create_bucket(self, bucket, *args, **kwargs):
+    bucket = self.new_bucket(bucket)
+    response = self.make_request(method='POST', path='/b',
+                                 data={'name': bucket.name},
+                                 expect_json=True)
     return Bucket.from_dict(response, connection=self)
 
-  def delete_bucket(self, bucket_name, *args, **kwargs):
-    response = self.make_request(method='DELETE', path='/b/%s' % bucket_name)
+  def delete_bucket(self, bucket, *args, **kwargs):
+    bucket = self.new_bucket(bucket)
+    response = self.make_request(method='DELETE', path=bucket.path,
+                                 expect_json=True)
     return True
+
+  def new_bucket(self, bucket):
+    if isinstance(bucket, Bucket):
+      return bucket
+
+    # Support Python 2 and 3.
+    try:
+      string_type = basestring
+    except NameError:
+      string_type = str
+
+    if isinstance(bucket, string_type):
+      return Bucket(connection=self, name=bucket)
+
+    raise TypeError('Invalid bucket: %s' % bucket)
